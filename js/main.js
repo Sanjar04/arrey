5a94ff5318fca02ade6ebe7e042665a3efcf5d82
Sanjar04 <sanjarpolatov200204@gmail.com>
2022-08-22 23:12:30 -0900

    arrey

---
+++ js/main.js
@@ -0,0 +1,268 @@
+"use strict";
+
+//====== 1-masala ======//
+
+// let n = 10;
+// let arr = [];
+
+// for(let i = 0; i <= n; i++){
+//    arr[i] = i;
+// }
+// console.log(arr)
+
+//====== 2-masala ======//
+
+// let n = 10;
+// let arr = [];
+
+// for(let i = 0; i <= n; i++){
+//    arr[i] = Math.pow(2 , i);
+// }
+// console.log(arr)
+
+//====== 3-masala ======//
+
+// let n = +prompt("n=");
+// let sum = 0;
+// let arr = [];
+
+// for (let i = 0; i <= n; i++) {
+//    sum = sum + i;
+
+// }
+// console.log(arr[sum]=sum)
+
+//====== 4-masala ======//
+
+// let arr = [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100];
+// let sum = 0;
+// let num = 0;
+
+// for(let i = 0; i < arr.length; i++){
+//    if(i % 2 == 0){
+//       sum = sum - arr[i]
+//          console.log(sum)
+//    }
+//    else{
+//       num = num + arr[i]
+//          console.log(num)
+//    }
+// }
+
+//====== 5-masala ======//
+
+// let n = 6;
+// let arr = [];
+// let sum = 0;
+
+// for(let i = 0; i <= n; i++){
+//    let pr = +prompt(`son kiriting ${i}`)
+//    arr[i] = pr;
+// }
+// for(let j = 0; j < arr.length; j++){
+//    sum += arr[j]
+// }
+// console.log(arr)
+// console.log(sum)
+
+//====== 6-masala ======//
+
+// let n = 8;
+// let arr = [];
+//  for(let i = 0; i <= n; i++){
+//    arr[i] = i;
+// }
+// arr.reverse()
+// console.log(arr)
+
+//====== 7-masala ======//
+
+// let array = ["Dilmurod", 17, "Muhammadali", 16,"Ozodbek", 19, "Hamid",24, "sanjar", 48];
+
+// console.log("Dilmurod"+array[1]+" yosh "+" Abdulla "+array[7]+" yosh "+" farqi "+" Dilmurod " +(24-17)+ " yosh kichik Hamid ")
+
+//====== 8-masala ======//
+
+// let sum = 0;
+// let arr = [];
+// let K = +prompt("K=");
+// let L = +prompt("L=");
+// for (let j = K; j < L; j++) {
+// sum += arr[j];
+// }
+// console.log(sum)
+
+//====== 9-masala ======//
+
+// let n = +prompt("n=");
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0) {
+//    sum = i
+//   } else {
+//     console.log();
+//   }
+// }
+// console.log(sum)
+
+//====== 10-masala ======//
+
+// let n = +prompt("n=");
+// let sum = 2;
+
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 == 0) {
+//    sum == Math.min(i)
+//   } else {
+//     console.log();
+//   }
+// }
+// console.log(sum)
+
+//====== 11-masala ======//
+
+// let n = +prompt("n=");
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 == 0) {
+//    console.log();
+//   } else {
+//      sum = (Math.max(i))
+//   }
+// }
+// console.log(sum)
+
+//====== 12-masala ======//
+
+// let sum = 0;
+// let max = 0;
+// let n = +prompt("nechi marta son kiritmoqchisiz: ");
+// for(let i = 1; i <= n; i++){
+//    let num = +prompt("Sonlarni kiriting: ");
+
+//    sum += num;
+//    max = i
+// }
+// console.log((sum/max)+" o'rta arifmetigi")
+
+//====== 13-masala ======//
+
+// let n = +prompt("n=");
+// let sum = 0;
+// let arr = [];
+
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 == 0) {
+//     console.log();
+//   } else {
+//     console.log(arr[i] = i);
+//   }
+// }
+
+//====== 14-masala ======//
+
+// let number = 9;
+
+// for(let i = 1; i <= number; i++){
+//    let dividedBySelf = i % i == 0;
+//    let dividedByOne = i % 1 == 0
+
+//    if(i == 1 || i == 2 || i == 3 || i == 5 || i == 7){
+//    }
+//    else if(dividedBySelf && dividedByOne && i % 2 == 0){
+//        console.log(i)  
+//    }
+//    else if(dividedBySelf && dividedByOne && i % 3 == 0){
+//       console.log(i)  
+//    }
+//    else if(dividedBySelf && dividedByOne && i % 5 == 0){
+//       console.log(i)  
+//    }
+//    else if(dividedBySelf && dividedByOne && i % 7 == 0){
+//       console.log(i)  
+//    }
+//    else if(dividedBySelf && dividedByOne && i % 2 == 0){  
+//    }
+// }
+
+//====== 15-masala ======//
+
+// let arr = [2 , 4 , 56 , 89 , 10 , 23 , 45 , 69];
+
+// for(let i = 0; i <= arr.linght; i++){
+//    arr[i] %  2 == 1 ? console.log(arr[i]) : console.log()
+// }
+
+//====== 16-masala ======//
+
+// let arr = [2 , 4 , 56 , 89 , '10' , 23 , '45' , 69 , 'string' , 'str'];
+
+// for(let i = 0; i <= arr.linght; i++){
+//    typeof arr[i] == typeof '' ? console.log(arr[i]) : console.log()
+// }
+
+//====== 17-masala ======//
+
+// let a = [1 , 4 , 5 , 9];
+// let b = [1 , 3 , 9];
+
+// function findSameNumbers(a , b){
+//    for(let i of a){
+//       for(let j of b){
+//          i == j ? console.log(i) : console.log()
+//       }
+//    }
+// }
+// findSameNumbers(a , b)
+
+//====== 18-masala ======//
+
+// let n = 6;
+// let arr = [];
+
+// for(let i = 0; i <= n; i++){
+//    let pr = +prompt(`son kiriting ${i}`)
+//    arr[i] = pr;
+// }
+
+//== 5-masala 19-masala bilan bir hil ==//
+
+//====== 20-masala ======//
+
+// let n = 8;
+// let arr = [];
+
+// for(let i = 0; i <= n; i++){
+//    let pr = +prompt(`sonni kiriting ${i}`)
+//    arr[i] = pr;
+// }
+// let isOddOrEvenNumbers = prompt('enter toq or juft !!!')
+
+// function isOdd(){
+//    for(let i = 0; i <= arr.linght; i++){
+//       arr[i] % 2 == 1 ?console.log(arr[i]) : []
+//    }
+// }
+// function isEven(){
+//    for(let i = 0; i <= arr.linght; i++){
+//       arr[i] % 2 == 0 ?console.log(arr[i]) : []
+//    }
+// }
+// if(isOddOrEvenNumbers == 'toq'){
+//    isOdd()
+// }else if(isOddOrEvenNumbers == 'juft'){
+//    isEven()
+// }else {
+//    console.log('error')
+// }
+
+//====== 21-masala ======//
+
+// let arr = [2, 4 , 56 , 89 , 10 , 23 , 45 , 69];
+
+// function findLastElement(){
+//    console.log(arr[arr.linght - 1])
+// }
+// findLastElement()
